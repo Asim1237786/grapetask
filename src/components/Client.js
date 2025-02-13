61adf42e443a481bbae191e02d5ee93f29b155c3
zarmeenkhan <zarmeenkhan833@gmail.com>
2025-02-13 21:28:14 +0500

    our experts
    our experts

--- src/components/Client.js
+++ src/components/Client.js
@@ -14,16 +14,16 @@ const Client = () => {
       <div className="container ">
         <div className="row justify-content-center text-center">
           <h3 className="font-40 font-500 cocon">
-            Our Experts Are Ready{" "}
+          Our Experts Are Here {" "}
             <br className="d-lg-block d-md-block d-none" />
-            To answer any questions
+            Ask Your Questions Now
           </h3>
           <div className="d-flex justify-content-center">
             <img src={line} className="text-center" alt="" />
           </div>
           <div className="col-lg-4 col-md-6 col-12 mt-lg-5 mt-md-4 mt-3">
             <div className="position-relative about-expert-befor">
-              <img src={imges1} className="w-100 rounded-2" alt="w8" />
+              <img src={imges1} className="w-100 rounded-2" alt="grapetask freelancer" />
               <div className="about-expert-images px-2 p-3">
                 <FiInstagram color="#1F2732" size={22} />
                 <FaFacebook className="mx-4" color="#1F2732" size={22} />
@@ -31,12 +31,12 @@ const Client = () => {
                 <BsPinterest color="#1F2732" className="ms-4" size={22} />
               </div>
             </div>
-            <h3 className="font-28 font-Garamond fw-bold mt-4">Michal David</h3>
-            <p className="font-18 colororing">Criminal Attorney</p>
+            <h4 className="font-28 font-Garamond fw-bold mt-4">Emily Roberts</h4>
+            <p className="font-18 colororing">Project Lead, Creative Agency</p>
           </div>
           <div className="col-lg-4 col-md-6 col-12 mt-lg-5 mt-md-4 mt-3">
             <div className="position-relative about-expert-befor">
-              <img src={imges2} className="w-100 rounded-2" alt="w8" />
+              <img src={imges2} className="w-100 rounded-2" alt="grapetask freelancer" />
               <div className="about-expert-images px-2 p-3">
                 <FiInstagram color="#1F2732" size={22} />
                 <FaFacebook className="mx-4" color="#1F2732" size={22} />
@@ -44,14 +44,14 @@ const Client = () => {
                 <BsPinterest color="#1F2732" className="ms-4" size={22} />
               </div>
             </div>
-            <h3 className="font-28 font-Garamond fw-bold mt-4">
-              Nikolona Gail
-            </h3>
-            <p className="font-18 colororing">Senior Attorney</p>
+            <h4 className="font-28 font-Garamond fw-bold mt-4">
+            David Thompson
+            </h4>
+            <p className="font-18 colororing">CEO, Tech Solutions</p>
           </div>
           <div className="col-lg-4 col-md-6 col-12 mt-lg-5 mt-md-4 mt-3">
             <div className="position-relative about-expert-befor">
-              <img src={imges3} className="w-100 rounded-2" alt="w8" />
+              <img src={imges3} className="w-100 rounded-2" alt="grapetask freelancer" />
               <div className="about-expert-images px-2 p-3">
                 <FiInstagram color="#1F2732" size={22} />
                 <FaFacebook className="mx-4" color="#1F2732" size={22} />
@@ -59,10 +59,10 @@ const Client = () => {
                 <BsPinterest color="#1F2732" className="ms-4" size={22} />
               </div>
             </div>
-            <h3 className="font-28 font-Garamond fw-bold mt-4">
-              Thomas Daniyel
-            </h3>
-            <p className="font-18 colororing">Civil Attorney</p>
+            <h4 className="font-28 font-Garamond fw-bold mt-4">
+            Sarah Ahmed
+            </h4>
+            <p className="font-18 colororing">Marketing Manager</p>
           </div>
           <div className=" mt-lg-5 mt-md-4 mt-3">
             <button className="btn-with-border px-5 font-20 fw-medium">
