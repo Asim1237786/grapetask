4c805334eeefa2e77d0ec5e2bdb167d595216b34
zarmeenkhan <zarmeenkhan833@gmail.com>
2025-02-18 13:54:21 +0500

    why graptask
    why graptask

--- src/pages/WhyGrapeTask.js
+++ src/pages/WhyGrapeTask.js
@@ -30,10 +30,8 @@ const WhyGrapeTask = () => {
         <div className="row h-100">
           <div className="col-lg-6 col-md-6 col-12 d-flex align-items-center justify-content-center px-lg-4 px-3">
             <h1 className="font-40 fw-medium cocon">
-              How Can Grape <span className="colororing"> Task</span> assist
-              <br className="d-xl-block d-none" /> you in Your Freelancing
-              <br className="d-lg-block d-none" />
-              Business?
+              How GrapeTask Can<span className="colororing">Help You Succeed</span>  
+              <br className="d-xl-block d-none" />in Freelancing?
             </h1>
           </div>
           <div className="col-lg-6 col-md-6 col-12">
