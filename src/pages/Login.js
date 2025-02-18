83f7c9687fe36da80bd1eb24f8b349e56ce03aa0
zarmeenkhan <zarmeenkhan833@gmail.com>
2025-02-19 02:24:22 +0500

    login
    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Loginleft from "../components/Loginleft";
 import Login1 from "../components/Login1";
+import CommonHelmet from './CommonHelmet';
 
 
 const Login = () => {
@@ -8,6 +9,12 @@ const Login = () => {
 
   return (
     <>
+     <CommonHelmet />
+                     <Helmet>
+                     <title>Grapetask Login </title>
+                     <meta name="description" content="GrapeTask is the go-to freelancing platform for businesses to grow and succeed with affordable, reliable freelance services"/>
+                       <link rel="canonical" href="https://www.grapetask.co/login" />
+                     </Helmet>
       {/*  */}
     
       <div className="container-fluid poppins min-100vh">
